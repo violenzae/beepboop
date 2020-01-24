559b9909aa34f1bd140357ea87108a2c80c136c2
Rachel Schieferstein <violenza@gmail.com>
2020-01-24 14:53:41 -0800

    adding an if statement that flags when a number is divisible by three and prints a user entered name

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("form#beepboop").submit(function(event){
   event.preventDefault();    
   var input = parseInt($("#num").val());
+  var name = $("#name").val();
   var array = [];
   var updown = parseInt($("input:radio[name=updown]:checked").val());
   
@@ -14,9 +15,11 @@ $(document).ready(function(){
     goDown(input);
   }
 
-  function goUp(input) {
+function goUp(input) {
   for(i=0; i <= input; i++){
-    if(i.toString().includes("3")) {
+    if(i % 3 === 0 && i !== 0){
+      array.push("I'm sorry, " +name+ ". I'm afraid I can't do that.")
+    } else if(i.toString().includes("3")) {
       array.push("I'm sorry, Dave. I'm afraid I can't do that.")
     } else if (i.toString().includes("2")){
       array.push("Boop!")
@@ -29,19 +32,20 @@ $(document).ready(function(){
 }
 
 function goDown(input) {
-  for(i=input; i >= 0; i--){
-    if(i.toString().includes("3")) {
+  for(i=0; i <= input; i++){
+    if(i % 3 === 0 && i !== 0){
+      array.push("I'm sorry, " +name+ ". I'm afraid I can't do that.")
+    } else if(i.toString().includes("3")) {
       array.push("I'm sorry, Dave. I'm afraid I can't do that.")
     } else if (i.toString().includes("2")){
       array.push("Boop!")
     } else if (i.toString().includes("1")){
       array.push("Beep!")
     } else {
-    array.push(i.toString())
+      array.push(i.toString())
     }
   }
 }
-
 
   array.forEach(function(item){
     $("#output").append('<li>' + item + '</li>');
