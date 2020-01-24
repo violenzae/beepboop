6fa6e235458afc4e1eb10ac18f4b2d0946f09bd8
Rachel Schieferstein <violenza@gmail.com>
2020-01-24 12:00:47 -0800

    added logic for deciding up or down and the for loop for counting down

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   event.preventDefault();    
   var input = parseInt($("#num").val());
   var array = [];
+  var updown = parseInt($("input:radio[name=updown]:checked").val());
 
 
   console.log(input); //just checking :(
@@ -10,6 +11,13 @@ $(document).ready(function(){
   
   $("#output").empty();
 
+  if (updown == 0) {
+    goUp(input);
+  } else if (updown == 1) {
+    goDown(input);
+  }
+
+  function goUp(input) {
   for(i=0; i <= input; i++){
     if(i.toString().includes("3")) {
       array.push("I'm sorry, Dave. I'm afraid I can't do that.")
@@ -21,7 +29,23 @@ $(document).ready(function(){
     array.push(i.toString())
     }
   }
-  
+}
+
+function goDown(input) {
+  for(i=input; i >= 0; i--){
+    if(i.toString().includes("3")) {
+      array.push("I'm sorry, Dave. I'm afraid I can't do that.")
+    } else if (i.toString().includes("2")){
+      array.push("Boop!")
+    } else if (i.toString().includes("1")){
+      array.push("Beep!")
+    } else {
+    array.push(i.toString())
+    }
+  }
+}
+
+
   array.forEach(function(val){
     $("#output").append('<li>' + val + '</li>');
   });
