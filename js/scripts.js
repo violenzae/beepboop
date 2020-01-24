58bdbedb1d43b74bfc8ae7fea02c58ac91a1dc13
Rachel Schieferstein <violenza@gmail.com>
2020-01-24 15:28:57 -0800

    adding a statement and loop to trigger a homer picture to replace the robot and for all numbers dividable by 2 as well as 0 to be replaced with 'doh'

--- js/scripts.js
+++ js/scripts.js
@@ -9,46 +9,59 @@ $(document).ready(function(){
   $("#output").empty();
   $("#gear").hide();
 
+  if (name.toLowerCase().includes("homer") === true){
+    $("#robot").hide();
+    $("#homer").show();
+  }
+
+
   if (updown == 0) {
     goUp(input);
   } else if (updown == 1) {
     goDown(input);
   }
 
-function goUp(input) {
-  for(i=0; i <= input; i++){
-    if(i % 3 === 0 && i !== 0){
-      array.push("I'm sorry, " +name+ ". I'm afraid I can't do that.")
-    } else if(i.toString().includes("3")) {
-      array.push("I'm sorry, Dave. I'm afraid I can't do that.")
-    } else if (i.toString().includes("2")){
-      array.push("Boop!")
-    } else if (i.toString().includes("1")){
-      array.push("Beep!")
-    } else {
-    array.push(i.toString())
-    }
-  }
-}
 
-function goDown(input) {
-  for(i=0; i <= input; i++){
-    if(i % 3 === 0 && i !== 0){
-      array.push("I'm sorry, " +name+ ". I'm afraid I can't do that.")
-    } else if(i.toString().includes("3")) {
-      array.push("I'm sorry, Dave. I'm afraid I can't do that.")
-    } else if (i.toString().includes("2")){
-      array.push("Boop!")
-    } else if (i.toString().includes("1")){
-      array.push("Beep!")
-    } else {
+
+  function goUp(input) {
+    for(i=0; i <= input; i++){
+      if(i % 3 === 0 && i !== 0){
+        array.push("I'm sorry, " +name+ ". I'm afraid I can't do that.")
+      } else if(i.toString().includes("3")) {
+        array.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      } else if (i.toString().includes("2")){
+        array.push("Boop!")
+      } else if (i.toString().includes("1")){
+        array.push("Beep!")
+      } else if (i % 2 === 0 && name.toLowerCase().includes("homer") === true) {
+        array.push("d'oh.")
+      } else {
       array.push(i.toString())
+      }
     }
   }
-}
+
+  function goDown(input) {
+    for(i=0; i <= input; i++){
+      if(i % 3 === 0 && i !== 0){
+        array.push("I'm sorry, " +name+ ". I'm afraid I can't do that.")
+      } else if(i.toString().includes("3")) {
+        array.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      } else if (i.toString().includes("2")){
+        array.push("Boop!")
+      } else if (i.toString().includes("1")){
+        array.push("Beep!")
+      } else {
+        array.push(i.toString())
+      }
+    }
+  }
 
   array.forEach(function(item){
     $("#output").append('<li>' + item + '</li>');
+
+
+
   });
 
   });
